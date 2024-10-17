926c0b54176b59dc6f425307001c8cbff855c66d
Gia Huy <tovugiahuy@gmail.com>
2024-10-17 18:51:30 +0700

    Initial commit

---
+++ screens/Screen02.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Screen02 = () => {
+  return (
+    <View>
+      <Text>Screen02</Text>
+    </View>
+  )
+}
+
+export default Screen02
+
+const styles = StyleSheet.create({})
